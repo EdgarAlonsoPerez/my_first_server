ba02f201131ceff14d16f31a410dba361a2d8415
Logan <ingperezedgar@gmail.com>
2022-02-16 12:33:17 -0700

    added paths to the server

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import http from 'http'
+
 const PORT = 3000;
 
+let locations = [{ city: "Ciudad Juarez", state: "Chihuahua", country: "Mexico", id: "1" }, { city: "EL Paso", state: "Texas", country: "United States", id: "2" }];
+let customers = [{ name: "Total Play", id: "1" }, { name: "Televisa", id: "2" }, { name: "UTEP", id: "3" }];
+
+const urlHandler = {
+    "/locations": (p) => {
+        return locations
+    },
+    "/customers": (p) => {
+        return customers
+    },
+    "null": () => {
+        return { message: 'Hello Logan' }
+    }
+}
+
 const server = http.createServer((req, res) => {
+    let result = urlHandler['null']()
+    if (req.url != '' && urlHandler.hasOwnProperty(req.url)) {
+        result = urlHandler[req.url](req)
+    }
+
     res.writeHead(200, {
         'Content-Type': 'application/json'
     })
-    res.end(JSON.stringify({ message: 'Hello Logan' }));
+
+    res.end(JSON.stringify(result));
 });
 
 server.listen(PORT, () => {
